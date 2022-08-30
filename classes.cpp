5acbb6aa6cc7f3d9cdac178a23ec691327e6dd7f
DefKirch <thijmen.kirch@gmail.com>
2022-08-30 23:19:17 +0200

    Added classes file

---
+++ classes.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+
+
+class Dog {
+  private:
+    std::string name;
+    int age;
+    const std::string kind;
+    const int amount_of_legs;
+
+  public:
+    // Dog() {
+    //   name = "Ollie";
+    //   age = 3;
+    // }
+    // You can use this constructor as default constructor using default values for the constructor parameters
+    Dog(std::string nom = "Ollie", int num = 3) : kind("Mamal"), amount_of_legs(4) {
+      name = nom;
+      age = num;
+    }
+
+    void summary() {
+      std::cout << name << " is " << age << " years old.\n";
+      std::cout << "This animal is a " << kind << " and has " << amount_of_legs << " legs\n";
+     }
+
+     ~Dog() {
+      std::cout << "Removed object with name: " << name <<std::endl;
+     }
+};
+
+
+int main() {
+
+  Dog my_dog;
+  my_dog.summary();
+
+  Dog my_other_dog("Doc", 11);
+  my_other_dog.summary();
+  return 0;
+}
