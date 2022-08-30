c1667bf965be7998d19f6e4fc55044e7bcc7ad75
DEFKIRCH <thijmen.kirch@gmail.com>
2022-08-30 19:42:50 +0200

    Using namespaces

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World\n";
+using std::cout;
+using std::string;
+
+int main()
+{
+	string name = "Thijmen\n";
+	cout << "Hello " << name;
+	// cout << "Hello World\n";
+	return 0;
 }
