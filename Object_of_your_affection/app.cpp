6f4a838f02e8b6fe21f9f8512b998db404074f45
DefKirch <thijmen.kirch@gmail.com>
2022-08-31 22:29:00 +0200

    Added class and constructor

---
+++ Object_of_your_affection/app.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include "profile.hpp"
+
+int main() {
+  Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
+
+  std::cout << sam.view_profile();
+};
