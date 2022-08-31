923bf2e9f93166617270fc332356d8de887fe42f
DefKirch <thijmen.kirch@gmail.com>
2022-08-31 22:51:11 +0200

    Added new 'add_hobby' method

--- Object_of_your_affection/app.cpp
+++ Object_of_your_affection/app.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include <stdio.h>
 #include "profile.hpp"
 
 int main() {
   Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
+
+  sam.add_hobby("Coding");
+  sam.add_hobby("Reading books");
+  sam.add_hobby("Cooking");
 
   std::cout << sam.view_profile();
 };
--- Object_of_your_affection/profile.hpp
+++ Object_of_your_affection/profile.hpp
@@ -14,5 +14,5 @@ class Profile {
   public:
     Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them" );
     std::string view_profile();
-    
+    void add_hobby(std::string new_hobby);
 };
