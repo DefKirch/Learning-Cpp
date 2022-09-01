15254c65792cc67aeb71e94d5bb822dfc2d1a6a0
DefKirch <thijmen.kirch@gmail.com>
2022-09-01 22:24:39 +0200

    Added comments

--- data_structures/vectors.cpp
+++ data_structures/vectors.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 
 int main() {
-
+  // Vectors are dynamic in size and can contain one data type
   std::vector<char> initials = {'T', 'J'};
 
+  // Pushed a new element to the back of the vector
   initials.push_back('R');
 
   std::cout << initials.front() << "\n";
@@ -12,6 +13,8 @@ int main() {
   std::cout << initials.back() << "\n";
 
   std::cout << initials.size() << "\n";
+  
+  // Pops one element from the back of the vector, does not return its value
   initials.pop_back();
   std::cout << initials.size() << "\n";
 
